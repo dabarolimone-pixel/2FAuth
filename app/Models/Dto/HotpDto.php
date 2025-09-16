60e2a4717ad499a534584208c5ba2860e7e4e53b
dabarolimone-pixel <dabarolimone@gmail.com>
2025-09-16 21:09:05 +0300

    Delete app directory

--- app/Models/Dto/HotpDto.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models\Dto;
-
-class HotpDto extends OtpDto
-{
-    /* @var integer */
-    public int $counter;
-}
--- app/Services/LogoLib/LogoLibInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Services\LogoLib;
-
-interface LogoLibInterface
-{
-    public function getIcon(?string $serviceName, ?string $variant = null) : ?string;
-}
