c61c559f6afeef949142a0c5d3c7e0f7e1364049
dabarolimone-pixel <dabarolimone@gmail.com>
2025-09-16 21:08:04 +0300

    Delete resources directory

--- resources/js/helpers.js
+++
@@ -1,9 +0,0 @@
-import { useColorMode } from '@vueuse/core'
-
-export default {
-    // getPropertyName(obj, expression) {
-    //     var res = {};
-    //     Object.keys(obj).map(k => { res[k] = k; });
-    //     return expression(res);
-    // },
-}
--- resources/js/router/middlewares/noEmptyError.js
+++
@@ -1,9 +0,0 @@
-export default function noEmptyError({ to, next, nextMiddleware, stores }) {
-    const { notify } = stores
-
-    if (notify.err == null && ! to.query.err) {
-        // return to home if no err object is set to prevent an empty error message
-        next({ name: 'accounts' });
-    }
-    else nextMiddleware()
-}
